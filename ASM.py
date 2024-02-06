c8ef4e7752f4f060e9c1bf9498534098f9f1f2d6
Will Barbour <12778879+barbourww@users.noreply.github.com>
2024-02-06 10:02:57 -0600

    Update ASM.py

--- ASM.py
+++ ASM.py
@@ -3,20 +3,24 @@ import pandas as pd
 from tqdm import tqdm
 
 
+# Global parameters
 smooth_x_window = 0.1
 smooth_t_window = 36
 
-def beta_free_flow(x,t,x_s,t_s,x_win,t_win,c_free=-43):
+
+def beta_free_flow(x, t, x_s, t_s, x_win, t_win, c_free=-43):
     dt = t-t_s- 3600*(x-x_s)/c_free
     dx = x-x_s
     return np.exp(-(2*np.abs(dx)/x_win + 2*np.abs(dt)/t_win))
 
-def beta_cong_flow(x,t,x_s,t_s,x_win,t_win,c_cong=13):
+
+def beta_cong_flow(x, t, x_s, t_s, x_win, t_win, c_cong=13):
     dt = t-t_s- 3600*(x-x_s)/c_cong
     dx = x-x_s
     return np.exp(-(2*np.abs(dx)/x_win + 2*np.abs(dt)/t_win))
 
-def EGTF(x,t,smooth_x_window, smooth_t_window, speed_raw_df):
+
+def EGTF(x, t, smooth_x_window, smooth_t_window, speed_raw_df):
     speed = speed_raw_df[(np.abs(speed_raw_df.t - t)<=(smooth_t_window/2)) & (np.abs(speed_raw_df.x - x)<=(smooth_x_window/2))]
     speed = speed.copy()
     EGTF_v_free = 80
@@ -27,11 +31,11 @@ def EGTF(x,t,smooth_x_window, smooth_t_window, speed_raw_df):
     if((sum(speed.beta_free)!=0) & (sum(speed.beta_cong)!=0)):
         EGTF_v_free = sum(speed.beta_free * speed.speed) / sum(speed.beta_free)
         EGTF_v_cong = sum(speed.beta_cong * speed.speed) / sum(speed.beta_cong)
-#         print(EGTF_v_free,EGTF_v_cong)
     v = min(EGTF_v_free,EGTF_v_cong)
     tanh_term = np.tanh((36-v) / 12.43)
     w = 0.5*(1+tanh_term)
     return w*EGTF_v_cong + (1-w)*EGTF_v_free
+
 
 def smooth_raw_data(speed_raw, dx, dt, smooth_x_window = 0.15, smooth_t_window = 36):
     EGTF_speed = pd.DataFrame(speed_raw)
